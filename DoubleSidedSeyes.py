c9b28d16650ba3b522c3e6242d6d07327a7b6f4d
Roch Schanen <r.schanen@lancaster.ac.uk>
2023-12-16 19:48:36 +0000

    add CahierDeTexte.ps

--- DoubleSidedSeyes.py
+++ DoubleSidedSeyes.py
@@ -1,10 +1,11 @@
 # file: DoubleSidedSeyes.py
 
-from localtools import exitProcess
-from localtools import hexcolor
-from localtools import psDoc
+from postscript.postscript import exitProcess
+from postscript.postscript import hexcolor
+from postscript.postscript import document
 
 ### SEYES RULES AND HOLES FOR A SINGLE A5 SIZE PAGE ###
+
 def seyes(lm = 0, rm = 0, tm = 0, bm = 0, vn = 13):
     p.rgbcolor(*hexcolor("c8c8de"))
     p.thickness(0.199)
@@ -16,11 +17,14 @@ def seyes(lm = 0, rm = 0, tm = 0, bm = 0, vn = 13):
     topmargin, lines = t-20-3*2, 22
     p.hgrid(topmargin,  topmargin -(lines-1)*8, lines, lm, rm)
     # vertical main grid
-    leftmargin, lines = l+44, vn
+    leftmargin, lines = l+44+8*2, vn-2
     p.vgrid(leftmargin, leftmargin+(lines-1)*8, lines, tm, bm)
     # vertical margin
-    p.rgbcolor(*hexcolor("f6bbcf"))
+    # p.rgbcolor(*hexcolor("f6bbcf"))
     p.vline(l+36, tm, bm)
+    # second vertical margin
+    # p.rgbcolor(*hexcolor("f6bbcf"))
+    p.vline(l+36+8*2, tm, bm)
     # done
     return
     
@@ -38,20 +42,20 @@ def page_number(n):
     return
 
 # import tool 
-from localtools import scl
+from postscript.postscript import sca
 
 def page_border(m, w):
     lm, rm, tm, bm = 0, 0, 0.3, 0
     # define  block
     BLOCK = f'''
     % --- CURVE BREAK ---
-    {scl(l+w+lm, t-w-tm)} moveto
-    {scl(l+w+lm, b+w+bm)} lineto
-    {scl(r-m, b+w+bm)} lineto
-    {scl(r-m/2, b+w+bm, r-w-rm, b+m/2, r-w-rm, b+m)} curveto
-    {scl(r-w-rm, t-m)} lineto
-    {scl(r-w-rm, t-m/2, r-m/2, t-w-tm, r-m, t-w-tm)} curveto
-    {scl(l+w+lm, t-w-tm)} lineto
+    {sca(l+w+lm, t-w-tm)} moveto
+    {sca(l+w+lm, b+w+bm)} lineto
+    {sca(r-m, b+w+bm)} lineto
+    {sca(r-m/2, b+w+bm, r-w-rm, b+m/2, r-w-rm, b+m)} curveto
+    {sca(r-w-rm, t-m)} lineto
+    {sca(r-w-rm, t-m/2, r-m/2, t-w-tm, r-m, t-w-tm)} curveto
+    {sca(l+w+lm, t-w-tm)} lineto
     closepath
     stroke
     '''
@@ -64,9 +68,9 @@ def rightIndent(pos, w, h):
     # define  block
     BLOCK = f'''
     % --- RIGHT INDENT ---
-    {scl(r, pos+h/2)} moveto
-    {scl(r, pos, r-w, pos, r-w, pos-h/2)} curveto
-    {scl(r-w, b)} lineto
+    {sca(r, pos+h/2)} moveto
+    {sca(r, pos, r-w, pos, r-w, pos-h/2)} curveto
+    {sca(r-w, b)} lineto
     stroke
     '''
     # export text
@@ -74,7 +78,7 @@ def rightIndent(pos, w, h):
     # done
     return
 
-p = psDoc("./doublesidedseyes.ps", Format = "A5")
+p = document("./CahierDeTexte", Size = "A5", Type = "ps")
 
 l, r, t, b, w = p.LEFT, p.RIGHT, p.TOP, p.BOTTOM, 0.099
 
@@ -92,16 +96,19 @@ for page in range(7):
     for i in range(1, 7-page):
         rightIndent((t-b)/7*(i-7/2), 8, 8)
 
+    p.graycolor(0.5)
     for i, txt in zip(
             range(0, 7-page),
-            ["Dimanche", "Samedi", "Vendredi", "Jeudi",
-            "Mercredi", "Mardi", "Lundi"]):
+            ["Dimanche", "Samedi",
+             "Vendredi", "Jeudi",
+             "Mercredi", "Mardi",
+             "Lundi"]):
         p.vtext(r-3, (t-b)/7*(i-7/2)+6, txt)
 
+    p.rgbcolor(*hexcolor("c8c8de"))
     p.thickness(w)
     leftholes()
 
 p.graycolor(1.0)
 
 exitProcess("end-of-code.")
-
